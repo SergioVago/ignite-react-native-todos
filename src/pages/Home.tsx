1f59cb0fe608efd5bf46682786c13ef46175d410
SergioVago <sergio.vago@hotmail.com>
2022-08-22 12:40:21 -0300

    Desafio finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,24 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      done: false,
+      title: newTaskTitle
+    }
+
+    setTasks((oldTasks) => [...oldTasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks((oldTasks) => oldTasks.map((task) => task.id === id ? {
+      ...task,
+      done: !task.done
+    } : task))
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks((oldTasks) => oldTasks.filter(task => task.id !== id))
   }
 
   return (
